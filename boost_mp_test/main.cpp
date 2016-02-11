31e722c85e744a9ec7cf2065745355e2ddbf05f7
mino2357 <mino2357@gmail.com>
2016-02-12 03:29:57 +0900

    add

--- boost_mp_test/main.cpp
+++ boost_mp_test/main.cpp
@@ -6,8 +6,13 @@ namespace mp = boost::multiprecision;
 int main()
 {
 	mp::cpp_dec_float_100 x = 2.0f;
+	mp::cpp_dec_float_100 y;
 
 	mp::cpp_dec_float_100 result = mp::sqrt(x);
+
+	y = result;
+
+	std::cout << y << std::endl;
 
 	std::cout << std::setprecision(std::numeric_limits<decltype(x)>::digits10 + 1)
 			<< result;
