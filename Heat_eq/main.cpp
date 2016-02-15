c0513ec6f6a3a1e991b49bb52e529d82165eb5b4
mino2357 <mino2357@gmail.com>
2016-02-16 02:05:53 +0900

    add Heat_eq

---
+++ Heat_eq/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include "../library/Linear_Algebra.hpp"
+#include "../library/ODE_solver.hpp"
+#include "../library/ratio.hpp"
+
+using namespace std;
+
+namespace mp = boost::multiprecision;
+using namespace mp;
+typedef mp::cpp_dec_float_100 f100;
+
+#define TYPE f100
+
+const int dim = 1;
+
+int main(){
+
+
+	return 0;
+}
